e7260d0760dd2240ac06a63223e576ae42e597a9
Yan <yan_patrick24@hotmail.com>
2025-08-13 22:41:30 +0000

    Desafio nível mestre concluído

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -17,14 +17,69 @@ int main(){
     tabuleiro[4][9] = 3;
 
     //Posição do navio 3 (Diagonal)
-    tabuleiro[7][5] = 3;
-    tabuleiro[8][4] = 3;
-    tabuleiro[9][3] = 3;
+    tabuleiro[7][7] = 3;
+    tabuleiro[8][8] = 3;
+    tabuleiro[9][9] = 3;
 
     //Posicação do navio 4 (Diagonal)
-    tabuleiro[5][1] = 3;
-    tabuleiro[6][2] = 3;
-    tabuleiro[7][3] = 3;
+    tabuleiro[5][7] = 3;
+    tabuleiro[6][8] = 3;
+    tabuleiro[7][9] = 3;
+
+    // For que cria a habilidade em cone
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 0) {
+                tabuleiro[4][2] = 1;
+            }
+            if (i == 1) {
+                tabuleiro[5][1] = 1;
+                tabuleiro[5][2] = 1;
+                tabuleiro[5][3] = 1;
+            }
+            if (i == 2) {
+                tabuleiro[6][0] = 1;
+                tabuleiro[6][1] = 1;
+                tabuleiro[6][2] = 1;
+                tabuleiro[6][3] = 1;
+                tabuleiro[6][4] = 1;
+            }
+        }
+    }
+    //For que cria a habilidade em octaedo
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 0) {
+                tabuleiro[1][6] = 1;
+            }
+            if (i == 1) {
+                tabuleiro[2][5] = 1;
+                tabuleiro[2][6] = 1;
+                tabuleiro[2][7] = 1;
+            }
+            if (i == 2) {
+                tabuleiro[3][6] = 1;
+            }
+        }
+    }
+    //For que cria a habilidade em cruz
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 0) {
+                tabuleiro[7][4] = 1;
+            }
+            if (i == 1) {
+                tabuleiro[8][2] = 1;
+                tabuleiro[8][3] = 1;
+                tabuleiro[8][4] = 1;
+                tabuleiro[8][5] = 1;
+                tabuleiro[8][6] = 1;
+            }
+            if (i == 2) {
+                tabuleiro[9][4] = 1;
+            }
+        }
+    }
 
     printf("=== Tabuleiro ===\n");
     for (int i = 0; i < 10; i++) {
